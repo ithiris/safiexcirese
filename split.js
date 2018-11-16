8ce0743f4ea5e0a09e4b664bb05c84a2585d4011
ithiris <ithirisshabi@gmail.com>
2018-11-16 16:19:25 +0530

    splitintoarray

---
+++ split.js
@@ -0,0 +1,18 @@
+
+var str ="hello";
+
+var splitarr =str.split();
+console.log(splitarr);
+
+
+//this with string loop//
+	
+var string ="world"
+var res =[]
+for (var i=0;i<string.length;i++){
+
+res.push(string[i])
+console.log(res);
+}
+
+ 
