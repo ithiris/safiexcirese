26f864cbd706da5815786ea1a5d39142d2838e8f
ithiris <ithirisshabi@gmail.com>
2018-11-18 20:28:26 +0530

    splitintoarray

---
+++ javascriptexcirese/splitintoarray.js
@@ -0,0 +1,14 @@
+
+
+
+
+var convertintoarray ="Robin Singh"
+array =[]
+function string_to_array(convertintoarray){
+	
+	array=convertintoarray.split(" ");
+ 
+
+ return array;	
+}
+console.log(string_to_array("Robin Singh"));
